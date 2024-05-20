7297f4bc5eb62a6a58a73000a21b59e3dfc8f37d
MasamichiKanakubo <renlijinjiubao808@gmail.com>
2024-05-20 15:54:53 +0900

    first commit

---
+++ tests/test_blog_scraping.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+import os #OS関連の機能へのアクセスやファイルパスの操作など、さまざまなOS関連の機能を提供
+
+url = 'https://www.hinatazaka46.com/s/official/diary/detail/55957?ima=0000&cd=member'
+response = requests.get(url)
+web_content = response.text
+soup = BeautifulSoup(web_content, 'html.parser')
+
+image_elements = soup.find_all('img') #このリストの中には'img'の要素が入っており，.jpg以外の拡張子の画像も含まれる
+directory_name = 'haruyo_yamaguchi'# ディレクトリの名前を決める
+if not os.path.exists(directory_name):
+    os.makedirs(directory_name)
+
+image_count = 1
+# すべての画像要素を処理して保存
+for img in image_elements: #リスト内の画像要素でループ
+    image_url = img['src']  # 'src'属性から画像のURLを取得
+    if image_url.endswith('.jpg'):
+        image_response = requests.get(image_url)# 画像をダウンロード
+        
+        filename = f'{directory_name}/haruyo_yamaguchi{image_count}.jpg' # ファイル名を決定
+        # ローカルに保存
+        with open(filename, 'wb') as f: #バイナリーモード
+                                        #'rd'はバイナリーモードで読み込み
+            f.write(image_response.content)
+            print(f"{filename}を保存しました") #保存したことを表示
+        # イメージカウンターを増やす
+        image_count += 1
