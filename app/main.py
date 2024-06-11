d6b7406cfb19acb720ceb8891125820cb9cae2dc
Shoma <shoma@nakanoshoubanoMacBook-Air.local>
2024-06-11 19:37:04 +0900

    feat: add cors

--- app/main.py
+++ app/main.py
@@ -2,31 +2,48 @@ import shutil
 
 from app.google_photo.google_photo import GooglePhotoFacade
 from app.scraping.blog_scraping import HinatazakaBlogScraper
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 OFFISIAL_BLOG_URL = 'https://www.hinatazaka46.com/s/official/diary/member'
 HOME_URL = "https://www.hinatazaka46.com"
 TARGET_MEMBER_NAME = "山口 陽世"
 
-if __name__ == "__main__":
-    google_photo = GooglePhotoFacade(
-        credential_path="credentials.json", token_path="token.pkl"
-    )
-    
-    scraper = HinatazakaBlogScraper(OFFISIAL_BLOG_URL)
-    
-    name_blog_dict = scraper.fetch_latest_author_dict()
-    
-    if TARGET_MEMBER_NAME not in name_blog_dict:
-        print("更新情報はありません")
-        
-    url = HOME_URL + name_blog_dict[TARGET_MEMBER_NAME]
-    print(url)
-    src_count = scraper.get_image_from_method(url)
+app = FastAPI()
 
-    for i in range(1,src_count - 1):
-        google_photo.upload(
-            local_file_path=f"haruyo_yamaguchi/haruyo_yamaguchi{i}.jpg", # ここに保存する画像を指定する。
-            album_id="AGpp2nYJaO5iOon6PNbFy1VIkM9BFmflNtIGlt-Lujn5mD2oTolE7PaARc0kpq0WMh5RUAUL0I6N"
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+@app.get("/")
+async def haruyo_scrape():
+    try:
+        google_photo = GooglePhotoFacade(
+            credential_path="credentials.json", token_path="token.pkl"
         )
         
-    shutil.rmtree("haruyo_yamaguchi")
+        scraper = HinatazakaBlogScraper(OFFISIAL_BLOG_URL)
+        
+        name_blog_dict = scraper.fetch_latest_author_dict()
+        
+        if TARGET_MEMBER_NAME not in name_blog_dict:
+            print("更新情報はありません")
+            
+        url = HOME_URL + name_blog_dict[TARGET_MEMBER_NAME]
+        print(url)
+        src_count = scraper.get_image_from_method(url)
+
+        for i in range(1,src_count - 1):
+            google_photo.upload(
+                local_file_path=f"haruyo_yamaguchi/haruyo_yamaguchi{i}.jpg", # ここに保存する画像を指定する。
+                album_id="AGpp2nYJaO5iOon6PNbFy1VIkM9BFmflNtIGlt-Lujn5mD2oTolE7PaARc0kpq0WMh5RUAUL0I6N"
+            )
+            
+        shutil.rmtree("haruyo_yamaguchi")
+        return {"Response": "Blog scraped successfully"}
+    except:
+        return {"Error": "something went wrong"}
